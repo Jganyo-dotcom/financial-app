d3bb54ec057a155c1df3a1294b477a4f88d8334b
Jganyo-dotcom <elikemejay@gmail.com>
2026-09-15 09:56:13 +0000

    eloy

--- src/components/apiEnpoint.jsx
+++ src/components/apiEnpoint.jsx
@@ -1,2 +1,2 @@
 //export const API_BASE_URL = "http://localhost:4444"; 
-export const API_BASE_URL = "https://albiappbackend.onrender.com";
+export const API_BASE_URL = "https://jojo-albijo-backend-app.onrender.com";
